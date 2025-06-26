21e63d28e575dec68766eb49f10cbf92005cfdf5
Marcos Gil <marcosgildeoliveira@gmail.com>
2025-06-26 12:03:45 -0300

    added code and figure for wave on a string example

--- wave_on_string.jl
+++ wave_on_string.jl
@@ -1,29 +1,66 @@
-using CairoMakie, FFTW
+using CairoMakie
 
-N = 1024
-L = 1.0
-Δx = L / N
-xs = (0:N-1) * Δx
-c = 1.0
-
-u0 = exp.(-((xs .- 0.5 * L) ./ 0.1) .^ 2)
-u̇0 = 2c * xs .* u0 / 2
-ks = fftfreq(N, 2π / Δx)
-
-u̇0 ./ ks
-
-val = u0 .- im * u̇0 ./ ks / c
-val[1] = 0.0  # Set the DC component to zero to avoid division by zero
-
-αs = fft(val)
-βs = fft(conj(val))
-
-ts = (0:N-1) * Δx / c
-result = zeros(N, N)
-
-for n ∈ axes(result, 2)
-    result[:, n] = real(bfft(circshift(αs, n - 1)) +
-                        bfft(circshift(βs, 1 - n)))
+function f(x, x₀, σ)
+    exp(-((x - x₀) / σ)^2)
 end
 
-lines(result[:, 9])
+# Boundary condition types
+struct Dirichlet end
+struct Neumann end
+
+phase(::Dirichlet, cond, val) = cond ? val : -val
+phase(::Neumann, cond, val) = val
+
+function evaluate(func, x, L, bc, args...)
+    y = mod(x, 2L)
+    cond = y < L
+    y = cond ? y : 2L - y
+    val = func(y, args...)
+    phase(bc, cond, val)
+end
+
+# Full D'Alembert solution
+function sol(x, t, c, L, bc, args...)
+    a = x - c * t
+    b = x + c * t
+
+    f1 = evaluate(f, a, L, bc, args...)
+    f2 = evaluate(f, b, L, bc, args...)
+    (f1 + f2) / 2
+end
+
+
+N = 256
+L = 1.0
+Δx = L / N
+xs = (0:N) * Δx
+c = 1.0
+
+t = Observable(0.0)
+
+ts = LinRange(0, 1, 256)
+figtitle = @lift("t=" * string(round($t, digits=2)))
+
+u_dir = @lift([sol(x, $t, 1, 1, Dirichlet(), 0.5, 0.05) for x in xs])
+u_neu = @lift([sol(x, $t, 1, 1, Neumann(), 0.5, 0.05) for x in xs])
+
+fig = with_theme(theme_latexfonts()) do
+    fig = Figure(; size=(500, 450), fontsize=24)
+    ax1 = Axis(fig[2, 1:3], xlabel=L"x", ylabel=L"u(x, t)", title=L"\text{Dirichlet B.C.: } u(0, t) = u(L, t) = 0")
+    ax2 = Axis(fig[3, 1:3], xlabel=L"x", ylabel=L"u(x, t)", title=L"\text{Neumann B.C.: } \partial_x u(0, t) = \partial_x u(L, t) = 0")
+    ylims!(ax1, -1.1, 1.1)
+    ylims!(ax2, -1.1, 1.1)
+    lines!(ax1, xs, u_dir, color=:blue, linewidth=4)
+    lines!(ax2, xs, u_neu, color=:red, linewidth=4)
+    Label(fig[1, :], figtitle)
+    fig
+end
+##
+t[] = 0.55
+fig
+##
+
+##
+record(fig, "test.mp4", ts; framerate=32) do s
+    t[] = s
+end
